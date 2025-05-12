5b39e2514e0c5f14a0e55f2611a6e3e0ec733430
anatol-coder <ankawysota@gmail.com>
2025-05-12 06:29:14 +0100

    Added features to Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -3,17 +3,20 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
+    console.log(props.photos);
     return (
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <img
-                  src={photo.src.landscape}
-                  className="img-fluid"
-                  alt={photo.alt}
-                />
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
+                </a>
               </div>
             );
           })}
